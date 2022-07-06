a402dcc18f25c4125aa959e1690b5cd7b4d0e142
Dylan Allart <chenoa.299@gmail.com>
2022-07-06 17:11:30 +0200

    add button and more

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  return (<div className='container'>
+    <div className='app-wrapper'>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Form
+          input={input}
+          setInput={setInput}
+          todo={todo}
+          setTodo={setTodo}
+          editTodo={editTodo}
+          setEditTodo={setEditTodo}
+        />
+      </div>
+      <div>
+        <TodoList todo={todo} setTodo={setTodo} setEditTodo={setEditTodo}/>
+      </div>
     </div>
-  );
+  </div>);
 }
 
 export default App;
